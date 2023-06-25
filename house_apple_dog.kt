8a3913b7b5647a6d0cae655a46d6f2f0708916e1
syedanas591 <136756050+syedanas591@users.noreply.github.com>
2023-06-25 16:25:08 +0700

    Enhance error handling

---
+++ house_apple_dog.kt
@@ -0,0 +1,248 @@
+class HappyHounds {
+    // Properties of the HappyHounds class 
+    var breed: String? 
+    var size: String
+    var age: Int 
+    var color: String
+  
+    // Constructor of the HappyHounds class 
+    constructor(breed: String?, size: String, age: Int, color: String) {
+        this.breed = breed
+        this.size = size
+        this.age = age
+        this.color = color
+    }
+  
+    // Function to display the details of HappyHounds 
+    fun displayDetails(){ 
+        println("Breed: $breed") 
+        println("Size: $size") 
+        println("Age: $age") 
+        println("Color: $color") 
+    } 
+  
+    // Function to run when a HappyHounds object is created 
+    init { 
+        println("A new HappyHound has been created!") 
+    } 
+  
+    // Function to bark 
+    fun bark(){ 
+        println("Bark! Bark!") 
+    } 
+  
+    // Function to get groomed 
+    fun getGroomed(){ 
+        println("Getting groomed!") 
+    } 
+  
+    // Function to fetch a ball 
+    fun fetchBall(){
+        println("Fetching a ball!") 
+    }
+  
+    // Function to jump 
+    fun jump(){ 
+        println("Jumping!") 
+    }
+  
+    // Function to eat 
+    fun eat(){ 
+        println("eating!") 
+    }
+  
+    // Function to sleep 
+    fun sleep(){ 
+        println("Sleeping zzz") 
+    }
+  
+    // Function to play 
+    fun play(){ 
+        println("Playing!") 
+    } 
+  
+    // Function to play fetch 
+    fun playFetch(){ 
+        println("Playing Fetch!") 
+    } 
+  
+    // Function to go for a walk 
+    fun goForAWalk(){ 
+        println("Going for a walk!") 
+    }
+  
+    // Function to run 
+    fun run(){ 
+        println("Running!") 
+    }
+  
+    // Function to play hide and seek 
+    fun playHideAndSeek(){ 
+        println("Playing hide and seek!") 
+    }
+  
+    // Function to play tug of war 
+    fun playTugOfWar(){ 
+        println("Playing tug of war!") 
+    }
+  
+    // Function to get a bath 
+    fun getBath(){ 
+        println("Getting a bath!") 
+    } 
+  
+    // Function to go for a swim 
+    fun goForASwim(){ 
+        println("Going for a swim!") 
+    }
+  
+    // Function to participate in agility 
+    fun participateInAgility(){ 
+        println("Participating in Agility!") 
+    }
+  
+    // Function to learn tricks 
+    fun learnTricks(){ 
+        println("Learning tricks!") 
+    }
+  
+    // Function to play catch 
+    fun playCatch(){ 
+        println("Playing catch!") 
+    }
+  
+    // Function to play Frisbee 
+    fun playFrisbee(){ 
+        println("Playing Frisbee!") 
+    }
+  
+    // Function to cuddle 
+    fun cuddle(){ 
+        println("Cuddling!") 
+    }
+  
+    // Function to rollover 
+    fun rollover(){ 
+        println("Rolling over!") 
+    } 
+  
+    // Function to sit 
+    fun sit(){ 
+        println("Sitting!") 
+    } 
+  
+    // Function to lie down 
+    fun lieDown(){ 
+        println("Lying down!") 
+    }
+  
+    // Function to stay 
+    fun stay(){ 
+        println("Staying!") 
+    }
+  
+    // Function to play musical chairs 
+    fun playMusicalChairs(){ 
+        println("Playing musical chairs!") 
+    }
+  
+    // Function to howl 
+    fun howl(){ 
+        println("Howling!") 
+    } 
+  
+    // Function to run in circles 
+    fun runInCircles(){ 
+        println("Running in circles!") 
+    } 
+  
+    // Function to play with toys 
+    fun playWithToys(){ 
+        println("Playing with toys!") 
+    }
+  
+    // Function to receive treats 
+    fun receiveTreats(){ 
+        println("Receiving treats!") 
+    }
+ 
+    // Function to explore around 
+    fun exploreAround(){ 
+        println("Exploring around!") 
+    }
+  
+    // Function to lick 
+    fun lick(){ 
+        println("Licking!") 
+    }
+  
+    // Function to chase tails 
+    fun chaseTails(){ 
+        println("Chasing tails!") 
+    }
+  
+    // Function to eat grass 
+    fun eatGrass(){ 
+        println("Eating grass!") 
+    }
+  
+    // Function to show off tricks 
+    fun showOffTricks(){ 
+        println("Showing off tricks!") 
+    }
+  
+    // Function to go for car rides 
+    fun goForCarRides(){ 
+        println("Going for car rides!") 
+    }
+  
+    // Function to take naps 
+    fun takeNaps(){ 
+        println("Taking naps!") 
+    }
+  
+    // Function to listen to music 
+    fun listenToMusic(){ 
+        println("Listening to music!") 
+    }
+  
+    // Function to watch TV 
+    fun watchTV(){ 
+        println("Watching TV!") 
+    }
+  
+    // Function to ride a bike  
+    fun rideABike(){ 
+        println("Riding a bike!") 
+    }
+  
+    // Function to repair something 
+    fun repairSomething(){ 
+        println("Repairing something!") 
+    }
+  
+    // Function to carry something 
+    fun carrySomething(){ 
+        println("Carrying something!") 
+    }
+  
+    // Function to participate in obedience 
+    fun participateInObedience(){ 
+        println("Participating in obedience!") 
+    }
+  
+    // Function to go on hikes 
+    fun goOnHikes(){ 
+        println("Going on hikes!") 
+    }
+  
+    // Function to go on trips 
+    fun goOnTrips(){ 
+        println("Going on trips!") 
+    }
+  
+    // Function to meet new people 
+    fun meetNewPeople(){ 
+        println("Meeting new people!") 
+    }
+}
